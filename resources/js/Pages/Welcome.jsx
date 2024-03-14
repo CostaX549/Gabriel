e6208ed1c779dab8d66bd874951559a032c0ec6f
CostaX549 <costasantos896@gmail.com>
2024-03-14 14:36:29 -0300

    reconhecimentoFacial

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import { Link, Head } from '@inertiajs/react';
+import React, { useState, useRef, useEffect } from 'react';
+
+export default function Welcome({ auth, laravelVersion, phpVersion }) {
+ 
+  
+    return (
+      <div>
+       <a href="/login">Login</a>
+      </div>
+    );
+}
