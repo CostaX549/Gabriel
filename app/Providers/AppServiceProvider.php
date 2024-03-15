5a705f62a29a2afb7fdc219e9760c474f25938c6
CostaX549 <costasantos896@gmail.com>
2024-03-14 21:44:17 -0300

    Mudança Perfeita

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        date_default_timezone_set('America/Sao_Paulo');
+       if(env('APP_ENV') !== 'local') {
+        URL::forceScheme('https');
+       }
     }
 }
